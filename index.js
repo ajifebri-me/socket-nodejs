f78a01a6dcd2f87b23614d9401e52123af7434cc
ajifebri-me <fanto356@gmail.com>
2022-09-28 10:00:37 +0700

    privat channel and add comment introduction

--- index.js
+++ index.js
@@ -7,23 +7,53 @@ const io = require("socket.io")(http,{
     }
 })
 
-//start socket server
-let connected;
+/**
+ * Create Connection Socket Server
+ */
+let connectedSocket;
 io.on("connection",socket => {
-    connected = socket
+    connectedSocket = socket
     socket.on("message", param => {
         io.emit("message", param)
     })
+
+    socket.on("private message", (anotherSocketId, msg) => {
+        socket.to(anotherSocketId).emit("private message", socket.id, msg);
+    });
 })
 
+/**
+ * Middleware for accept request body
+ */
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
-// endpoint for emit message
+/**
+ * Endpoint for emit socket server
+ */
+
+//public channel
 app.post("/emit", function(request, response){
     try {
-        connected.emit("message", request.body)
+        connectedSocket.emit("message", request.body)
     
+        response.status(200).json({
+            status: true,
+            message: "send message successs",
+        })
+    } catch (error) {
+        response.status(500).json({
+            status: false,
+            message: "send message failed"
+        })
+    }
+})
+
+//private channel
+app.post("/emit/privat-channel", function(request, response){
+    try {
+        connectedSocket.emit("private message", 2, "response 1")
+
         response.status(200).json({
             status: true,
             message: "send message successs",
